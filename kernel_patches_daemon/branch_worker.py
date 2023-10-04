cf3bce4e3808b83c39be159b0ac86769549b9a3b
Björn Töpel <bjorn@rivosinc.com>
2023-10-04 17:46:44 +0200

    Set the suite status based on the test results
    A Github action can only fail/succeed. Current, if any of the tests
    has failed, we mark the whole PR as failed.
    
    Here, we try to be a bit smarter. The conclusion is based on if we
    have errors or errors. If there's a an error, red failure mark for
    each patch. Dito for waring,
    
    Also, only update the suite if it's in state success/failed.
    
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- kernel_patches_daemon/branch_worker.py
+++ kernel_patches_daemon/branch_worker.py
@@ -323,6 +323,9 @@ def scrape_workflow_run_results(logs: List[str]) -> Dict:
     reg = ".*##.*](OK|FAIL|WARN).*Patch (\d?\d?\d+)/.*Test (\d?\d?\d+)/\d?\d?\d+: (.*)"
     p = re.compile(reg)
     res = {}
+    err = 0
+    warn = 0
+    ok = 0
     for i in logs:
         m = p.match(i)
         if m:
@@ -332,17 +335,28 @@ def scrape_workflow_run_results(logs: List[str]) -> Dict:
             test = m.group(4)
             if patchnum not in res:
                 res[patchnum] = {}
-            if testnum not in res[patchnum]:
+            if testnum in res[patchnum]:
                 logger.warning(f"Multiple tests with same id: patch {patchnum}: test: {testnum}: {test}")
             res[patchnum][testnum] = {}
             if status == "OK":
                 state = "success"
+                ok += 1
             elif status == "WARN":
                 state = "warning"
+                warn += 1
             else:
                 state = "failure"
+                err += 1
             res[patchnum][testnum]["status"] = state
             res[patchnum][testnum]["test"] = test
+
+    if err:
+        res["conclusion"] = "failure"
+    elif warn:
+        res["conclusion"] = "warning"
+    elif ok:
+        res["conclusion"] = "success"
+
     return res
 
 
@@ -1006,13 +1020,8 @@ class BranchWorker(GithubConnector):
                 conclusion = suite.conclusion
                 break
 
-        logger.info(f"Check suite status: overall: '{conclusion}'")
-        await self.submit_pr_summary(
-            series=series,
-            state=conclusion,
-            context_name=ctx,
-            target_url=pr.html_url,
-        )
+        if conclusion != "success" and conclusion != "failure":
+            return
 
         logger.info("Fetching per-patch results")
 
@@ -1020,6 +1029,14 @@ class BranchWorker(GithubConnector):
         if len(res) == 0:
             logger.info(f"No checks found for {pr.number}: {pr.head.ref}")
             return
+
+        logger.info(f"Check suite status: overall: '{conclusion}'")
+        await self.submit_pr_summary(
+            series=series,
+            state=res["conclusion"],
+            context_name=ctx,
+            target_url=pr.html_url,
+        )
 
         patch_num = 1
         for i in series.patches:
